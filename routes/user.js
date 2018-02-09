7468c34ee751011143b2797df8494a701fb6e8f5
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2018-02-08 17:16:06 -0700

    new user signup form ui and new route

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
+// INDEX
+router.get('/', (req, res) => {
+	res.redirect('/');
+});
+
+// NEW - signup page
+router.get('/signup', (req, res) => {
+	res.render('user/signup');
+});
+
+// CREATE
+router.post('/', (req, res) => {
+	
+});
+
+// SHOW
 router.get('/:id', (req, res) => {
 	res.render('user/profile');
 });
+
+
+
+
+
+// EDIT
+
+// UPDATE
+
+// DELETE
 
 module.exports = router;
