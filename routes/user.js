d3dd181f47cb2adad8980fa8d3458feeea797bd0
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2018-02-08 17:54:04 -0700

    ui for login page and route for login page

--- routes/user.js
+++ routes/user.js
@@ -7,29 +7,38 @@ router.get('/', (req, res) => {
 	res.redirect('/');
 });
 
-// NEW - signup page
-router.get('/signup', (req, res) => {
-	res.render('user/signup');
-});
-
-// CREATE
-router.post('/', (req, res) => {
-	
-});
-
 // SHOW
-router.get('/:id', (req, res) => {
-	res.render('user/profile');
-});
-
-
-
-
+// router.get('/:id', (req, res) => {
+// 	res.render('user/profile');
+// });
 
 // EDIT
 
 // UPDATE
 
 // DELETE
+
+
+// Auth Routes
+
+// NEW - register form
+router.get('/register', (req, res) => {
+	res.render('user/register');
+});
+
+// CREATE - create new user
+router.post('/register', (req, res) => {
+	// TODO: kzd -> finish creating authentication route 
+});
+
+// LOGIN - login form
+router.get('/login', (req, res) => {
+	res.render('user/login');
+});
+
+//LOGIN - post authentication
+
+// LOGOUT
+
 
 module.exports = router;
