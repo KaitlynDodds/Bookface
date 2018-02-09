dfbcc96b16ee3868dc257395b11f0f33d382d0ee
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2018-02-09 09:33:31 -0700

    Setting up app to handle passport authentication

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-const methodOverride = require('method-override'),
-	  bodyParser = require('body-parser'),
-	  mongoose = require('mongoose'),
-	  express = require('express');
+const passportLocalMongoose = require('passport-local-mongoose');
+	  methodOverride 		= require('method-override'),
+	  expressSession 		= require('express-session');
+	  localStrategy 		= require('passport-local'),
+	  bodyParser 			= require('body-parser'),
+	  mongoose 				= require('mongoose'),
+	  passport 				= require('passport'),
+	  express 				= require('express');
 
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
 
 const BookModel = require('./models/book');
+const UserModel = require('./models/user');
 
 const seedDatabase = require('./seed.js');
-
-const app = express();
-mongoose.connect('mongodb://127.0.0.1/bookface_app');
-seedDatabase();
 
 
 // ***********
 // APP SETUP
 // ***********
+const app = express();
+mongoose.connect('mongodb://127.0.0.1/bookface_app');
+seedDatabase();
 app.set('view engine', 'ejs');
+// encode/decode session
+app.use(expressSession({
+	secret: "Books can transport you to a whole new world.",
+	resave: false,
+	saveUninitialized: false
+}));
+// passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+// read (deserialize) and encode (serialize) session 
+passport.serializeUser(UserModel.serializeUser());
+passport.deserializeUser(UserModel.deserializeUser());
+
 app.set(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + "/public"));
