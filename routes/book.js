c0763f00dbb73abc4900e6cab46e0087dd2f0887
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2018-02-09 12:53:44 -0700

    user profile route now set up

--- routes/book.js
+++ routes/book.js
@@ -3,17 +3,9 @@ const express = require('express');
 const Book = require('../models/book');
 const User = require('../models/user');
 
-const helperScripts = require('../public/scripts/scripts');
-const Authentication = require('./middleware/authentication');
+const isLoggedIn = require('./middleware/authentication');
 
 const router = express.Router();
-
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect('/');
-}
 
 router.get('/', isLoggedIn, (req, res) => {
 	res.render('book/feed', {isLoggedIn: true, user: req.user});
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const passport = require('passport');
+
 const router = express.Router();
+
 const User = require('../models/user');
+const isLoggedIn = require('./middleware/authentication');
 
 // INDEX
 router.get('/', (req, res) => {
-	res.redirect('/');
+	res.redirect('/logout');
 });
 
 // SHOW
-// router.get('/:id/profile', (req, res) => {
-// 	res.render('user/profile');
-// });
+router.get('/:id/profile', isLoggedIn, (req, res) => {
+	User.findById(req.params.id, (err, user) => {
+		if (err) {
+			console.log('error: ', err);
+			res.redirect('/logout'); // FIXME: kzd -> secure?
+		} else {
+			res.render('user/profile', {isLoggedIn: isLoggedIn, user: user});
+		}
+	});	
+});
 
 // EDIT
 
